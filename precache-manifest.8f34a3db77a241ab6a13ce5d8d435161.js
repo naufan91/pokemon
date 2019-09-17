39fd5f31e17be548d596badfb49f634ea94cdd51
Naufan Harish Kemara Jati <harish_naufan@hotmail.com>
2019-09-17 23:04:51 +0700

    Updates

---
+++ precache-manifest.8f34a3db77a241ab6a13ce5d8d435161.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f3638e28be170a6d9108480d72219c6",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "4884522f48a6967279bd",
+    "url": "/pokemon/static/css/main.22405dbe.chunk.css"
+  },
+  {
+    "revision": "3002ac362464e7c3b3b0",
+    "url": "/pokemon/static/js/2.446e007e.chunk.js"
+  },
+  {
+    "revision": "4884522f48a6967279bd",
+    "url": "/pokemon/static/js/main.fd092c48.chunk.js"
+  },
+  {
+    "revision": "c7b34e90fd5a4298f876",
+    "url": "/pokemon/static/js/runtime~main.9f7d124e.js"
+  },
+  {
+    "revision": "3cfd5e85cd2597eb00d7f5b0139d3b6d",
+    "url": "/pokemon/static/media/logo.3cfd5e85.svg"
+  }
+]);
